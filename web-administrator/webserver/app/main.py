7228749bec55d9b17dbb2acb965bd01959720a86
Nguyen Hoang Anh Tuan <tuantoquq@gmail.com>
2023-01-14 21:25:58 +0700

    refactor chart

--- web-administrator/webserver/app/main.py
+++ web-administrator/webserver/app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import sqlalchemy as db
+from pydantic import BaseModel 
 
 DB_USER = os.getenv('DB_USER', 'root')
 DB_PASS = os.getenv('DB_PASS', 'Admin123**')
@@ -12,6 +13,19 @@ DB_NAME = os.getenv('DB_NAME', 'manager')
 DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = db.create_engine(url=DB_URL)
 connection = engine.connect()
+meta = db.MetaData()
+users_table = db.Table('users', 
+                meta, 
+                db.Column('id', db.Integer, primary_key=True),
+                db.Column('name', db.String(256)),
+                db.Column('age', db.Integer),
+                mysql_engine='InnoDB',
+                mysql_charset='utf8')
+meta.create_all(engine)
+
+class User (BaseModel):
+    name: str
+    age: int
 
 app = FastAPI()
 app.add_middleware(
@@ -22,9 +36,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/hello")
-async def sample_get():
-    test_table = db.Table('test',db.MetaData(), autoload=True, autoload_with=engine)
-    sample_records = db.select(test_table)
+@app.get("/get-all")
+async def get_all():
+    sample_records = db.select(users_table)
     data = connection.execute(sample_records).fetchall()
-    return {"status": 0, "message": "Successfully!", "data": data}
+    return {"status": 0, "message": "Get all user successfully!", "data": data}
+
+@app.get("/get/{user_id}")
+async def get_by_id(user_id: int):
+    sample_records = db.select(users_table).where(users_table.c.id==user_id)
+    data = connection.execute(sample_records).fetchone()
+    return {"status": 0, "message": "Get user successfully!", "data": data}
+
+@app.post("/create")
+async def create(user: User):
+    sql = (db.insert(users_table).values(name=user.name, age=user.age))
+    connection.execute(statement=sql)
+    return {"status": 0, "message": "Create user successfully!"}
+        
+
+@app.delete('/delete/{user_id}')
+async def delete(user_id: int):
+    sql = db.select(users_table).where(users_table.c.id==user_id)
+    data = connection.execute(statement=sql).fetchone()
+    if data is None:
+        raise HTTPException(status_code=400, detail="User not found with input id")
+    else: 
+        sql = db.delete(users_table).where(users_table.c.id==user_id)
+        connection.execute(statement=sql)
+        return {"status": 0, "message": "Delete user successfully!"}
